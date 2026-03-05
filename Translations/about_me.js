f22829470ea702e1b27aa0ee36008ea036dec052
dannarcis24 <dannarcis.costinel@gmail.com>
2026-03-05 22:04:51 +0200

    Update CV and translations for improved clarity and accuracy

--- Translations/about_me.js
+++ Translations/about_me.js
@@ -1,23 +1,34 @@
-dict = {
+const dict = {
     ro: {
-        "home": "Acasa",
+        "home": "Acasă",
         "about-me": "Despre mine",
         "projects": "Proiecte",
         "title-about-me": "Despre mine",
-        "desc1": `Salut! Mă numesc Dan Narcis Costinel și sunt student în anul al doilea la <b>FACULTATEA DE AUTOMATICA
-                SI CALCULATOARE</b>, parte din Universitatea Națională de Știință și Tehnologie București,
-                specializarea <b>INGINERIA SISTEMELOR</b>.`,
-        "desc2": `Încă din gimnaziu am dezvoltat o pasiune pentru matematică, fiind atras de faptul că
-                rezolvarea problemelor presupune <b>GANIDRE ANALITICA</b> și un strop de creativitate. 
-                În liceu am ales profilul <b>MATEMATICA SI INFORMATICA</b>, unde am descoperit că îmi place
-                informatica pentru că mă ajută să îmi dezvolt gândirea analitică în rezolvarea problemelor complexe.`,
-        "desc3": `În clasa a 12-a m-am întrebat: <i>Ce facultate vreau să urmez?</i>, <i>Ce vreau să fac în viitor?</i>,
-                <i>Dacă aș continua cu matematica și informatica, ce opțiuni aș avea?</i> 
-                Am ales automatizările pentru că vreau să învăț despre <b>aplicabilitatea</b> lor în viața de zi cu zi,
-                iar ingineria sistemelor îmi oferă această posibilitate.`,
-        "desc4": `Studiile în domeniul <b>AUTOMATIZARILOR</b> și materiile parcurse până acum mi-au oferit
-                o înțelegere aprofundată a acestui domeniu vast, confirmând că am făcut <b>ALEGEREA
-                POTRIVITA</b>. Îmi doresc să contribui la soluții inovatoare și eficiente în viitor.`
+
+        "desc1": `Salut! Mă numesc Dan Narcis Costinel și sunt student în anul al doilea la 
+                <b>Facultatea de Automatică și Calculatoare</b>, parte din Universitatea Națională 
+                de Știință și Tehnologie București, specializarea <b>Ingineria Sistemelor</b>. 
+                Sunt pasionat de programare, algoritmi și dezvoltarea soluțiilor software pentru 
+                probleme tehnice complexe.`,
+
+        "desc2": `Interesul meu pentru domeniul tehnic a început încă din perioada liceului,
+                unde am urmat profilul <b>Matematică-Informatică</b>. Acolo am descoperit că
+                informatica îmbină perfect <b>gândirea analitică</b> cu rezolvarea creativă
+                a problemelor, ceea ce m-a motivat să continui studiile în domeniul
+                automatizărilor și ingineriei sistemelor.`,
+
+        "desc3": `În prezent sunt interesat de domenii precum <b>algoritmi și structuri de date</b>,
+                <b>optimizare</b>, <b>machine learning</b> și <b>sisteme de control</b>.
+                Pe parcursul studiilor am realizat proiecte variate, de la aplicații software
+                și sisteme embedded până la simulări numerice și jocuri dezvoltate în Unity.`,
+
+        "desc4": `Am fost voluntar ca <b>asistent de laborator</b> la disciplinele 
+                <b>Proiectarea Algoritmilor</b> și <b>Programare Orientată pe Obiecte</b>,
+                unde am ghidat studenții în implementarea structurilor de date, algoritmilor
+                pe grafuri și conceptelor de programare modernă în C++. 
+                Acest site reprezintă portofoliul meu tehnic și include o parte din proiectele
+                la care am lucrat.`
+
     },
 
     en: {
@@ -25,19 +36,29 @@ dict = {
         "about-me": "About me",
         "projects": "Projects",
         "title-about-me": "About me",
-        "desc1": `Hello! My name is Dan Narcis Costinel, and I am a second-year student at the
-                <b>FACULTY OF AUTOMATIC CONTROL AND COMPUTER SCIENCE</b>, part of the National University
-                of Science and Technology Bucharest, specializing in <b>SYSTEMS ENGINEERING</b>.`,
-        "desc2": `Since middle school, I have developed a passion for mathematics, being drawn to the fact that
-                solving problems required <b>ANALYTICAL THINKING</b> and a bit of creativity. In high school I chose the
-                <b>MATHEMATICS-INFORMATICS</b> profile, where I discovered that I enjoy computer science because it helps me further
-                develop my analytical thinking in resolving complex problems.`,
-        "desc3": `In 12th grade, I started questioning myself: <i>What faculty do I want to attend?</i>,
-                <i>What would I like to do in the future?</i>, <i>If I were to continue with mathematics and
-                computer science, what options do I have?</i> I decided to pursue the field of automation
-                because I want to learn about their <b>application</b> in everyday life, and systems engineering allows me to do so.`,
-        "desc4": `My studies in the field of <b>AUTOMATION</b> and the subjects I have taken so far have
-                given me a deeper understanding of this vast domain, confirming that I made the <b>RIGHT CHOICE</b>.
-                I aspire to develop my career in this field, contributing to innovative and efficient solutions in the future.`
-  }
+
+        "desc1": `Hello! My name is Dan Narcis Costinel and I am a second-year student at the 
+                <b>Faculty of Automatic Control and Computer Science</b>, part of the 
+                National University of Science and Technology Bucharest, specializing in 
+                <b>Systems Engineering</b>. I am passionate about programming, algorithms,
+                and developing software solutions for complex technical problems.`,
+
+        "desc2": `My interest in the technical field started during high school,
+                where I studied in the <b>Mathematics-Informatics</b> track.
+                That is where I discovered that computer science combines
+                <b>analytical thinking</b> with creative problem solving,
+                motivating me to pursue studies in automation and systems engineering.`,
+
+        "desc3": `Currently, I am interested in areas such as <b>algorithms and data structures</b>,
+                <b>optimization</b>, <b>machine learning</b>, and <b>control systems</b>.
+                During my studies I have worked on diverse projects ranging from
+                software applications and embedded systems to numerical simulations
+                and games developed with Unity.`,
+
+        "desc4": `I have also volunteered as a <b>teaching assistant</b> for the courses
+                <b>Algorithm Design</b> and <b>Object-Oriented Programming</b>, helping
+                students understand data structures, graph algorithms and modern C++
+                programming concepts. This website represents my technical portfolio
+                and showcases some of the projects I have worked on.`
+    }
 };
